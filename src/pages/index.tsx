e85c585e96f6c19fd4477126ebb75f8537374576
João Eduardo <joaoeduardoj920@gmail.com>
2023-02-01 16:13:12 -0300

    add getStactProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
+import { GetStaticProps } from 'next';
 import { Contact } from '../components/Contact';
 import { Features } from '../components/Features';
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 import { Welcome } from '../components/Welcome';
+
+export const getStaticProps: GetStaticProps = () => {
+
+	return {
+		props: {}
+	};
+};
 
 export default function Home() {
 	return (
