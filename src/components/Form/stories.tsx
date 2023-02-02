6acde13828796797d6d0b538c333b9470962a138
João Eduardo <joaoeduardoj920@gmail.com>
2023-02-02 11:20:12 -0300

    add ajust storyboook of forms

--- src/components/Form/stories.tsx
+++ src/components/Form/stories.tsx
@@ -1,12 +1,19 @@
-import { Meta, ComponentStory } from '@storybook/react';
+import { Meta, ComponentStory, } from '@storybook/react';
+import { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
-import { Form, } from '.';
+import { Form, FormProps } from '.';
 import { GlobalStyled } from '../../styles/globalStyles';
 import { theme } from '../../styles/theme';
+
+const [loading, setLonding] = useState(false);
 
 export default {
 	title: 'Form',
 	component: Form,
+	args: {
+		loading,
+		setLonding
+	} as unknown as FormProps,
 	decorators: [
 		(Story) => (
 			<ThemeProvider theme={theme}>
@@ -17,6 +24,6 @@ export default {
 	]
 } as Meta;
 
-export const Tamplate: ComponentStory<typeof Form> = () => {
-	return <div style={{ margin: '0 auto' }}><Form /></div>;
+export const Tamplate: ComponentStory<typeof Form> = (agrs: FormProps) => {
+	return <div style={{ margin: '0 auto' }}><Form {...agrs} /></div>;
 };
